2b7caee1636c83989eb0129caf320d89c7b913e7
Arlisson N. Costa <arlisson314@gmail.com>
2022-09-15 18:22:29 -0500

    feat: adiciona a funcão addProduct na camada de service

--- src/services/productServices.ts
+++ src/services/productServices.ts
@@ -1,6 +1,12 @@
 import productModel from '../models/productModel';
+import { IAddProduct } from '../types';
 
 const productService = {
+  async addProduct(name: IAddProduct['name'], amount: IAddProduct['amount']) {
+    const result = await productModel.addProduct(name, amount);
+    return { code: 201, data: result };
+  },
+
   async getAll() {
     const result = await productModel.getAll();
     return { code: 200, data: result };
