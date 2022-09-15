4b5aa2d1d4ec07028be5c55f2536b07b658cf105
Arlisson N. Costa <arlisson314@gmail.com>
2022-09-15 18:20:10 -0500

    feat: adiciona a funcão addProduct na mada de model

--- src/models/productModel.ts
+++ src/models/productModel.ts
@@ -1,11 +1,19 @@
-import { IgetProducts } from '../types';
+import { ResultSetHeader, RowDataPacket } from 'mysql2';
+import { IAddProduct, IgetProducts } from '../types';
 import connection from './connection';
 
 const TABLE = 'Trybesmith.Products';
 
 const productModel = {
+  async addProduct(name: IAddProduct['name'], amount: IAddProduct['amount']): Promise<IAddProduct> {
+    const sql = `INSERT INTO ${TABLE} (name, amount)`;
+    const [{ insertId }] = await connection.execute<ResultSetHeader>(sql, [name, amount]);
+    return { id: insertId, name, amount };
+  },
+
   async getAll(): Promise<IgetProducts[]> {
-    const [products] = await connection.execute(`SELECT id, name, amount, orderId FROM ${TABLE}`);
+    const sql = `SELECT id, name, amount, orderId FROM ${TABLE}`;
+    const [products] = await connection.execute<RowDataPacket[]>(sql);
     return products as IgetProducts[];
   },
 };
