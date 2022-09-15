88b4c69bf97140c096665933e8e12e4d1a91a421
Arlisson N. Costa <arlisson314@gmail.com>
2022-09-15 12:40:06 -0500

    feat: cria o endpoint get da rota porducts/

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
-import 'express-async-errors';
+// import 'express-async-errors';
+import productsRoutes from './routes/productRouts';
 
 const app = express();
-
 app.use(express.json());
+
+app.use('/products', productsRoutes);
 
 export default app;
