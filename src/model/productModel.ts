ecdf408d37a06983de26f6434e2fa2cb4a2e467a
Arlisson N. Costa <arlisson314@gmail.com>
2022-09-15 01:46:30 -0500

    feat: cria a camda productModel e a função getAll

--- src/model/productModel.ts
+++ src/model/productModel.ts
@@ -1,7 +1,13 @@
+import { IAddProduct } from '../types';
 import connection from './connection';
 
-export const productModel = {
-  async addProduct() {
-    
-  }
-}
+const TABLE = 'Trybesmith.Products';
+
+const productModel = {
+  async getAll(): Promise<IAddProduct[]> {
+    const [products] = await connection.execute(`SELECT id, name, amount, orderId FROM ${TABLE}`);
+    return products as IAddProduct[];
+  },
+};
+
+export default productModel;
