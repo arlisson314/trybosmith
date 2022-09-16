daa2498eb0cd4b0f969fe30c3e994cd25403db16
Arlisson N. Costa <arlisson314@gmail.com>
2022-09-15 20:06:14 -0500

    feat: adiciona a funcão addProduct na camada de controller

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -1,8 +1,13 @@
 import { Request, Response } from 'express';
 import productService from '../services/productServices';
-// import productService from '../services/productServices';
 
 const productsController = {
+  async addProduct(req: Request, res: Response) {
+    const { name, amount } = req.body;
+    const { code, data } = await productService.addProduct(name, amount);
+    return res.status(code).json(data);
+  },
+
   async getAll(_req: Request, res: Response) {
     const { code, data } = await productService.getAll();
     return res.status(code).json(data);
