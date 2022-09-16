6b86785ac93273135a99e820d188a1588b2b6646
Arlisson N. Costa <arlisson314@gmail.com>
2022-09-16 16:14:08 -0500

    refactor: alterando nomes de variaveis

--- src/models/productModel.ts
+++ src/models/productModel.ts
@@ -2,17 +2,17 @@ import { ResultSetHeader, RowDataPacket } from 'mysql2';
 import { IAddProduct, IgetProducts } from '../types';
 import connection from './connection';
 
-const TABLE = 'Trybesmith.Products';
+const PRODUCT_TABLE = 'Trybesmith.Products';
 
 const productModel = {
   async addProduct(name: IAddProduct['name'], amount: IAddProduct['amount']): Promise<IAddProduct> {
-    const sql = `INSERT INTO ${TABLE} (name, amount) VALUES (?, ?)`;
+    const sql = `INSERT INTO ${PRODUCT_TABLE} (name, amount) VALUES (?, ?)`;
     const [{ insertId }] = await connection.execute<ResultSetHeader>(sql, [name, amount]);
     return { id: insertId, name, amount };
   },
 
   async listProducts(): Promise<IgetProducts[]> {
-    const sql = `SELECT id, name, amount, orderId FROM ${TABLE}`;
+    const sql = `SELECT id, name, amount, orderId FROM ${PRODUCT_TABLE}`;
     const [products] = await connection.execute<RowDataPacket[]>(sql);
     return products as IgetProducts[];
   },
