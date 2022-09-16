aa5b5627a8c124b77cc86fab843564e4e2ee27e9
Arlisson N. Costa <arlisson314@gmail.com>
2022-09-15 23:30:04 -0500

    feat: cria o endpoint Users

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 // import 'express-async-errors';
 import productsRoutes from './routes/productRouts';
+import userRouter from './routes/userRouts';
 
 const app = express();
 app.use(express.json());
 
 app.use('/products', productsRoutes);
+app.use('/users', userRouter);
 
 export default app;
