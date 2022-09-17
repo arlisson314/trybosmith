1c77a359826a06dec80e97adf32d656ccdc7930c
Arlisson N. Costa <arlisson314@gmail.com>
2022-09-17 10:16:43 -0500

    feat: cria o endpoint GET para a rota /login

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import express from 'express';
-import orderRouts from './routes/orderRouts';
 // import 'express-async-errors';
 import productsRoutes from './routes/productRouts';
+import loginRouter from './routes/loginRouts';
+import orderRouts from './routes/orderRouts';
 import userRouter from './routes/userRouts';
 
 const app = express();
@@ -10,5 +11,6 @@ app.use(express.json());
 app.use('/products', productsRoutes);
 app.use('/users', userRouter);
 app.use('/orders', orderRouts);
+app.use('/login', loginRouter);
 
 export default app;
