6a794e107124e0eda70ed4094e4c11cfb74ea58f
Arlisson N. Costa <arlisson314@gmail.com>
2022-09-17 09:56:35 -0500

    refactor: refatora a funcção tokenGernerete para usar id do usuario

--- src/services/genereteToken.ts
+++ src/services/genereteToken.ts
@@ -1,8 +1,8 @@
 import jwt from 'jsonwebtoken';
 import { IUser } from '../types';
 
-const tokenGenerate = (username: IUser['username']) => {
-  const PAYLOAD = { username, admin: false };
+const tokenGenerate = (id: IUser['id']) => {
+  const PAYLOAD = { id, admin: false };
   const SECRET = 'Comed!4nt3';
   const CONFIG = { expiresIn: '7d' };
   const token = jwt.sign(PAYLOAD, SECRET, CONFIG);
