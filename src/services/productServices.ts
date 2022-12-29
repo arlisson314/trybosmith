513dcd381615d1ed803628aa98231e4561b4f719
Arlisson N. Costa <arlisson314@gmail.com>
2022-12-29 12:41:02 -0500

    feat: adiciona validaçãoes com o joy

--- src/services/productServices.ts
+++ src/services/productServices.ts
@@ -1,8 +1,18 @@
 import { StatusCodes } from 'http-status-codes';
+import Joi from 'joi';
 import productModel from '../models/productModel';
 import { IAddProduct } from '../types';
 
 const productService = {
+  async ValidateProducts(unknown: unknown): Promise<IAddProduct> {
+    const schema = Joi.object<IAddProduct>({
+      name: Joi.string().required().min(3),
+      amount: Joi.string().required().min(3),
+    });
+    const result = await schema.validateAsync(unknown);
+    return result;
+  },
+
   async addProduct(name: IAddProduct['name'], amount: IAddProduct['amount']) {
     const result = await productModel.addProduct(name, amount);
     return { code: StatusCodes.CREATED, data: result };
